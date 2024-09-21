3bdc6af23db6d98f14994e4caa2cb9e6c4ced7ab
izanro <izanro2005@gmail.com>
2024-09-21 18:13:43 +0200

    Primer clase del powerpoint intro

---
+++ clase11.cpp
@@ -0,0 +1,17 @@
+// el numero maximo que podemos meter en el int
+#include <iostream>
+#include <string>
+
+using namespace std ;
+int main(){
+  int a = 1;
+  char b[2560];
+  cin >> b;
+  cout << b[2] << endl ;
+  cout << a << endl ;
+  string g = "Hello world";
+ 
+  
+  cout.write(g.c_str(),a) << endl;
+  
+  }
